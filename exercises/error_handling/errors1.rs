5d585a5d3ae55a35911978ddfa41a7b4244f137c
tabby-02 <1753779141@qq.com>
2024-04-14 20:59:00 +0800

    day two

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -9,14 +9,19 @@
 // Execute `rustlings hint errors1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-pub fn generate_nametag_text(name: String) -> Option<String> {
+
+pub fn generate_nametag_text(name: String) -> Result<String,String> {
     if name.is_empty() {
         // Empty names aren't allowed.
-        None
+        Err("`name` was empty; it must be nonempty.".into())
+        
+        
     } else {
-        Some(format!("Hi! My name is {}", name))
+        let mut m=String::from("Hi! My name is ");
+        m+=&name;
+        Ok(m)
+        
     }
 }
 
