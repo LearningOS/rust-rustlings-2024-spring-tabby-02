57d8906f3237b62017958414d1f83a7d742c6521
tabby-02 <1753779141@qq.com>
2024-04-20 16:23:13 +0800

    day seven

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,14 +2,15 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
 
-#[derive(Debug)]
+
+// #[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Queue<T> {
     elements: Vec<T>,
 }
 
-impl<T> Queue<T> {
+impl<T:Clone> Queue<T> {
     pub fn new() -> Queue<T> {
         Queue {
             elements: Vec::new(),
@@ -44,7 +45,7 @@ impl<T> Queue<T> {
     }
 }
 
-impl<T> Default for Queue<T> {
+impl<T:Clone> Default for Queue<T> {
     fn default() -> Queue<T> {
         Queue {
             elements: Vec::new(),
@@ -55,27 +56,64 @@ impl<T> Default for Queue<T> {
 pub struct myStack<T>
 {
 	//TODO
+    len:i32,
 	q1:Queue<T>,
 	q2:Queue<T>
 }
-impl<T> myStack<T> {
+impl<T:Clone> myStack<T> {
     pub fn new() -> Self {
         Self {
 			//TODO
+            len:0,
 			q1:Queue::<T>::new(),
 			q2:Queue::<T>::new()
         }
     }
     pub fn push(&mut self, elem: T) {
+        self.q1.enqueue(elem);
+        self.len+=1;
         //TODO
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
+        if self.q1.is_empty(){
+            if self.q2.is_empty(){return Err("Stack is empty")}
+            self.len-=1;
+            loop{
+                match self.q2.dequeue(){
+                    Err(_)=>break,
+                    Ok(x)=>{
+                        let mut test=self.q2.clone();
+                        match test.dequeue(){
+                            Err(_)=>{return Ok(x);},
+                            Ok(_)=>{self.q1.enqueue(x)}
+                        }
+                        
+                    }
+                }
+            }
+        }else{
+            self.len-=1;
+            loop{
+                match self.q1.dequeue(){
+                    Err(_)=>break,
+                    Ok(x)=>{
+                        let mut test=self.q1.clone();
+                        match test.dequeue(){
+                            Err(_)=>return Ok(x),
+                            Ok(_)=>{self.q2.enqueue(x)}
+                        }
+                        
+                    }
+                }
+            }
+
+        }
 		Err("Stack is empty")
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+        0==self.len
     }
 }
 
